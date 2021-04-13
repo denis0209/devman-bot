b3e3b793306501adc06e1e658a5ae41b8581d55a
denis0209 <>
2021-04-13 11:29:56 +0000

    commit

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def Start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Выберите автомобиль: \n/Mercedes \n/Audi \n/BMW \n/Ferrari \n/Toyota ")
 def Mercedes(message):
-    img = open('/images/mercedes.jpg', 'rb')
+    img = open('images/mercedes.jpg', 'rb')
     bot.send_photo(message.chat.id, img)
 def Second(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Второй ответ")
@@ -37,7 +37,7 @@ import os
 if __name__ == '__main__':
   Start()
   Mercedes()
-  
+
 
 
 password = os.getenv("PASSWORD")
